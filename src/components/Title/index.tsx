43d1494b7fb640c82e9a73c656aa16ba894cd146
loopy-lim <loopy@jnu.ac.kr>
2024-04-18 19:11:21 +0900

    feat: add title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -2,8 +2,17 @@ import style from "./index.module.scss";
 
 interface TitleProps {
   title: string;
+  subtitle: string;
 }
 
-export function Title({ title }: TitleProps) {
-  return <h1 className={style.title}>{title}</h1>;
+export function Title({ title, subtitle }: TitleProps) {
+  return (
+    <div className={style.titleWapper}>
+      <img className={style.logo} src="/jnu-logo.svg" alt="jnu logo"></img>
+      <div>
+        <h1 className={style.title}>{title}</h1>
+        <h2 className={style.subtitle}>{subtitle}</h2>
+      </div>
+    </div>
+  );
 }
